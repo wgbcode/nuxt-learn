6106fe0ce787d1faa65fa207cfb8d3a1f20ed5bd
wuguangbin <wuguangbin@neware.com.cn>
2025-02-26 17:46:16 +0800

    使用NuxtLayout布局组件

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    style="
+      position: relative;
+      background-color: #1a1a1a;
+      width: 100%;
+      height: 100%;
+      display: flex;
+    "
+  >
+    <div style="min-width: 200px; height: 100%; background-color: #081421">
+      <ul>
+        <li class="test"><NuxtLink to="/">首页</NuxtLink></li>
+        <li class="test"><NuxtLink to="/page1">测试1</NuxtLink></li>
+        <li class="test"><NuxtLink to="/page2">测试2（服务器）</NuxtLink></li>
+        <li class="test"><NuxtLink to="/page3">测试3</NuxtLink></li>
+      </ul>
+    </div>
+    <div style="flex: 1; height: 100%">
+      <slot />
+    </div>
+  </div>
+</template>
